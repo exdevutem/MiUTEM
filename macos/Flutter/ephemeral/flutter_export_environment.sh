bfc922a2c060ada058f8bfcf03be812990823f05
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-10-15 23:34:49 -0300

    patch: actualización de archivos macos

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/fran/fvm/versions/3.24.3"
+export "FLUTTER_APPLICATION_PATH=/Users/fran/Development/Apps/miutem"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=3.0.0"
+export "FLUTTER_BUILD_NUMBER=142"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
