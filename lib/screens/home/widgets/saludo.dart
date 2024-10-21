9f176549c59e305ff0fc3b2adc45135f59c302e2
Tomás L <59000698+XhrdTLS@users.noreply.github.com>
2024-10-21 15:30:11 -0300

    Feat: Formato texto y Profile Screen (#8)
    * feat: Formato textos - ProfileScreen
    
    - Se agregan formato para los textos, correspondientes a lo que se tiene en Figma
    - Se agrega la screen Profile
    - Cambios menores en UI
    
    * patch: Orden en widgets de ProfileScreen
    
    * Se agregan nuevos archivos para ordenar los widgets
    
    * patch: resolución de conflictos y cambios solicitados
    
    ---------
    
    Co-authored-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

--- lib/screens/home/widgets/saludo.dart
+++ lib/screens/home/widgets/saludo.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:miutem/core/models/user/estudiante.dart';
 import 'package:miutem/core/services/auth_service.dart';
+import 'package:miutem/core/utils/style_text.dart';
 import 'package:miutem/core/utils/utils.dart';
 
 class Saludo extends StatelessWidget {
@@ -12,24 +12,21 @@ class Saludo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(let<int, String>(DateTime.now().hour, (hour) => hour >= 6 && hour < 12 ? '¡Buenos Días!' : (hour >= 12 && hour < 19 ? '¡Buenas Tardes!' : '¡Buenas Noches!')) ?? '¡Te damos la Bienvenida!',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Text(estudiante?.primerNombre ?? "",
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+          Text(let<int, String>(DateTime.now().hour, (hour) => hour >= 6 && hour < 12 ? '¡Buenos Días!,' : (hour >= 12 && hour < 19 ? '¡Buenas Tardes!,' : '¡Buenas Noches!,')) ?? '¡Te damos la Bienvenida!,',
+            style: StyleText.headline,
           ),
-          const Spacer(),
-          GestureDetector(
-            onTap: () async => Get.find<AuthService>().logout(context: context),
-            child: const Text("👋", style: TextStyle(fontSize: 48)),
+          Text(estudiante?.primerNombre ?? "",
+            style: StyleText.headline,
           ),
         ],
-      );
+      ),
+      const Spacer(),
+      const Text("👋", style: TextStyle(fontSize: 40)),
+    ],
+  );
 }
