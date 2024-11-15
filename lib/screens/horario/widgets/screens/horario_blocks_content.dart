9ff5de0d8332735f1ad39bc457296f3a62227f7c
ggonu20 <g.gonzalezn@utem.cl>
2024-11-15 17:22:52 -0300

    fix: git ignora

--- lib/screens/horario/widgets/screens/horario_blocks_content.dart
+++ lib/screens/horario/widgets/screens/horario_blocks_content.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
 import 'package:miutem/core/models/horario.dart';
 import 'package:miutem/core/utils/theme.dart';
 import 'package:miutem/screens/horario/widgets/widgets.dart';
