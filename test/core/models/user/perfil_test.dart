e9794ccf5d0d0529ebb90f8955bf32aaeb1653bc
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2026-03-10 22:58:18 -0300

    patch: patched tests

--- test/core/models/user/perfil_test.dart
+++ test/core/models/user/perfil_test.dart
@@ -7,12 +7,28 @@ void main() {
       expect(Perfil.values, contains(Perfil.estudiante));
     });
 
-    test('debería tener solo 1 valor', () {
-      expect(Perfil.values.length, 1);
+    test('debería tener el valor profesor', () {
+      expect(Perfil.values, contains(Perfil.profesor));
+    });
+
+    test('debería tener el valor funcionario', () {
+      expect(Perfil.values, contains(Perfil.funcionario));
+    });
+
+    test('debería tener solo 3 valores', () {
+      expect(Perfil.values.length, 3);
     });
 
     test('debería tener nombre "estudiante"', () {
       expect(Perfil.estudiante.name, 'estudiante');
+    });
+
+    test('debería tener nombre "profesor"', () {
+      expect(Perfil.profesor.name, 'profesor');
+    });
+
+    test('debería tener nombre "funcionario"', () {
+      expect(Perfil.funcionario.name, 'funcionario');
     });
   });
 }
