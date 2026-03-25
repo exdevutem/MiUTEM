3bbaeb5e138278fabef57e46292a86fa15132cbb
xsilvamo <aaronsilva.csl@gmail.com>
2026-03-25 02:45:42 -0300

    fix: dupplicated variable

--- lib/screens/home/widgets/acceso_rapido.dart
+++ lib/screens/home/widgets/acceso_rapido.dart
@@ -10,47 +10,50 @@ class AccesoRapido extends StatelessWidget {
   const AccesoRapido({super.key, this.estudiante});
 
   @override
-  Widget build(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text("¿Qué quieres hacer hoy?", style: Theme.of(context).textTheme.bodyMedium),
-      Space.extraSmall,
-      SizedBox(
-        height: 130,
-        child: ListView(
-          physics: const AlwaysScrollableScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          children: [
-            if (estudiante?.perfiles.contains(Perfil.estudiante) ?? false)
+  Widget build(BuildContext context) {
+    final isEstudiante = estudiante?.perfiles.contains(Perfil.estudiante) ?? false;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("¿Qué quieres hacer hoy?", style: Theme.of(context).textTheme.bodyMedium),
+        Space.extraSmall,
+        SizedBox(
+          height: 130,
+          child: ListView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            children: [
+              if (isEstudiante)
+                CardAccesoRapido(
+                  color: AppTheme.lightBlueCard,
+                  colorDark: AppTheme.darkBlueCard,
+                  label: "Horario",
+                  icon: AppIcons.timetable,
+                  onTap: () => visitarHorario(context),
+                ),
+              if (isEstudiante) HorizontalSpace.extraSmall,
               CardAccesoRapido(
-                color: AppTheme.lightBlueCard,
-                colorDark: AppTheme.darkBlueCard,
-                label: "Horario",
-                icon: AppIcons.timetable,
-                onTap: () => visitarHorario(context),
+                color: AppTheme.lightPurpleCard,
+                colorDark: AppTheme.darkPurpleCard,
+                label: "Notas",
+                icon: AppIcons.calculator,
+                fill: 0,
+                onTap: () => visitarNotas(context),
               ),
-            if (estudiante?.perfiles.contains(Perfil.estudiante) ?? false)
               HorizontalSpace.extraSmall,
-            CardAccesoRapido(
-              color: AppTheme.lightPurpleCard,
-              colorDark: AppTheme.darkPurpleCard,
-              label: "Notas",
-              icon: AppIcons.calculator,
-              fill: 0,
-              onTap: () => visitarNotas(context),
-            ),
-            HorizontalSpace.extraSmall,
-            CardAccesoRapido(
-              color: AppTheme.lightGreenCard,
-              colorDark: AppTheme.darkGreenCard,
-              label: "Novedades",
-              icon: AppIcons.updates,
-              fill: 0,
-              onTap: () => visitarNovedades(context),
-            ),
-          ],
+              CardAccesoRapido(
+                color: AppTheme.lightGreenCard,
+                colorDark: AppTheme.darkGreenCard,
+                label: "Novedades",
+                icon: AppIcons.updates,
+                fill: 0,
+                onTap: () => visitarNovedades(context),
+              ),
+            ],
+          ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
