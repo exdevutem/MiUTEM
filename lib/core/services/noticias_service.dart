7238dfb59682741671b0b8a8b94c30aceafc8d8c
xsilvamo <aaronsilva.csl@gmail.com>
2026-03-25 02:41:28 -0300

    fix: defensive cache

--- lib/core/services/noticias_service.dart
+++ lib/core/services/noticias_service.dart
@@ -17,7 +17,7 @@ class NoticiasService {
 
   Future<List<Noticia>> getNoticias({ bool forceRefresh = false }) async {
     if (!forceRefresh && _cachedNoticias != null) {
-      return _cachedNoticias!;
+      return List.from(_cachedNoticias!);
     }
 
     final hasta = DateTime.now().toUtc().toIso8601String();
