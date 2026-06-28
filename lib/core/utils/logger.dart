0bf916e154b027dd01c37b4ea1e98c70b1756157
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2026-06-28 03:10:55 -0400

    patch: ignorar crashlytics si es windows
    Aun no existe implementación de firebase crashlytics para windows, por lo que debemos ignorarlo

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,3 +1,4 @@
+import "dart:io";
 import "package:firebase_crashlytics/firebase_crashlytics.dart";
 import "package:logger/logger.dart";
 
@@ -7,12 +8,11 @@ final logger = Logger(
 );
 
 class _ConsoleOutput extends LogOutput {
-
   @override
   void output(OutputEvent event) {
     // Only send warning-level and above to Crashlytics to avoid leaking
     // sensitive request/response data (headers, credentials) from debug logs.
-    if (event.level.index >= Level.warning.index) {
+    if ((event.level.index >= Level.warning.index) && !(Platform.isWindows)) {
       FirebaseCrashlytics.instance.log(event.lines.join("\n"));
     }
     for (final line in event.lines) {
