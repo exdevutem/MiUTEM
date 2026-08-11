50ba83fcc9a781eccd988b7e95d41295dd48ead6
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2026-08-10 22:39:36 -0400

    style: se repara errores del análisis

--- lib/screens/home/actions/cargar_clases_de_hoy.dart
+++ lib/screens/home/actions/cargar_clases_de_hoy.dart
@@ -9,13 +9,13 @@ Future<List<BloqueHorario>?> cargarClasesDeHoy({ bool forceRefresh = false }) as
     final horario = await Get.find<HorarioService>().getHorario(forceRefresh: forceRefresh);
     final diaIdx = ahora().weekday - 1;
     if(diaIdx < 0 || diaIdx >= (horario.horario?.first.length ?? 0)) {
-      return Future.error("No tienes clases hoy.");
+      throw CustomException(message: "No tienes clases hoy.");
     }
 
     final clasesDeHoy = horario.horario?.map((row) => row[diaIdx]).toList();
     final bloques = (clasesDeHoy?.asMap().entries.where((entry) => entry.key % 2 == 0).map((entry) => entry.value).toList())?.toList();
     if (clasesDeHoy == null || bloques?.where((bloque) => bloque.asignatura != null).isEmpty == true) {
-      return Future.error("No tienes clases hoy.");
+      throw CustomException(message: "No tienes clases hoy.");
     }
 
     return bloques;
